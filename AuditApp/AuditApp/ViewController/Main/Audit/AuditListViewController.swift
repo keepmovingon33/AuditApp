f915f508128d74cecd37fb199716f58c3ee5eb4a
sky <anvanc37@gmail.com>
2020-11-25 10:38:45 -0500

    doing Audit list screen with RxSwift

--- AuditApp/AuditApp/ViewController/Main/Audit/AuditListViewController.swift
+++ AuditApp/AuditApp/ViewController/Main/Audit/AuditListViewController.swift
@@ -7,35 +7,65 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+// doing normal way
+//class AuditListViewController: UIViewController {
+//
+//    @IBOutlet weak var tableView: UITableView!
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        dataBinding()
+//    }
+//
+//    func dataBinding() {
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1))
+//    }
+//
+//}
+//
+//extension AuditListViewController: UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 9
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "AuditItemTableViewCell", for: indexPath) as! AuditItemTableViewCell
+//        cell.dataBinding()
+//
+//        return cell
+//    }
+//}
+
+// doing RxSwift
+
 
 class AuditListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
+    let disposeBag = DisposeBag()
+    let items = Observable.of([1,2,3,4,5,6,7,8,9])
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         dataBinding()
+        
     }
     
     func dataBinding() {
-        tableView.delegate = self
-        tableView.dataSource = self
         tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1))
+        items.bind(to: tableView.rx.items(cellIdentifier: "AuditItemTableViewCell")) { row, model, cell in
+            if let cell = cell as? AuditItemTableViewCell {
+                cell.dataBinding()
+            }
+         }.disposed(by: disposeBag)
     }
 
-}
-
-extension AuditListViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 9
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AuditItemTableViewCell", for: indexPath) as! AuditItemTableViewCell
-        cell.dataBinding()
-        
-        return cell
-    }
 }
