ad77b193c9f19e27a794df0f40928e4c3e1894a1
sky <anvanc37@gmail.com>
2020-11-28 19:58:24 -0500

    Finish statistics view at Score Summary Screen

---
+++ AuditApp/AuditApp/ViewController/Main/Audit/ScoreSummaryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreSummaryViewController.swift
+//  AuditApp
+//
+//  Created by Sky on 11/28/20.
+//  Copyright © 2020 Sky. All rights reserved.
+//
+
+import UIKit
+
+class ScoreSummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
